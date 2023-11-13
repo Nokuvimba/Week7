af488d68f0e94991731dd62ab10d86cc4a21b6ea
Nokuvimba <G00423951@atu.ie>
2023-11-13 10:18:12 +0000

    made the while code infinite

--- src/main/java/ie/atu/UsingTryCatch.java
+++ src/main/java/ie/atu/UsingTryCatch.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class UsingTryCatch {
     public static void main(String[] args){
-        int num=1;
 
-        while(num!=0) {
+
+        while(true) {
             try {
                 System.out.println("Please enter a number between 0-9:");
                 Scanner myscan = new Scanner(System.in);
@@ -15,7 +15,7 @@ public class UsingTryCatch {
 
                 if (myNum >= 0 && myNum <= 9) {
                     System.out.println("You entered " + myNum);
-                    num=0;
+                    break;
                 } else {
                     System.out.println("Not a valid number");
                 }
